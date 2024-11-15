0fc79010a9840c9425b121048a6a1daa537defd5
Zefirfy <its.demtin@gmail.com>
2024-11-15 11:16:30 +0400

    release-1.1.1.0

---
+++ js/pages/lastNews.js
@@ -0,0 +1,16 @@
+fetch(links["news"])
+.then( news => news.json() )
+.then ( news => {
+    var news_wrapper = document.getElementById("news-wrapper")
+    console.log(news)
+
+
+    for (var i = 0; i < news.length; i++) {
+        console.log(news[i])
+        news_wrapper.innerHTML = "<div class='news-post'>" + news[i] + "</div>" + news_wrapper.innerHTML
+
+        if (i == 4) {
+            break
+        }
+    }
+})
